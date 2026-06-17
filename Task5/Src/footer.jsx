ceee0a9081e35cd15707a5031a5a65b81deaa01e
peswarmunikumar-create <peswarmunikumar@gmail.com>
2026-06-17 21:03:12 +0530

    Add files via upload

---
+++ Task5/Src/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      © 2026 Jayakar
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Task5/Src/navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Eswar</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Courses</li>
+        <li>Resources</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
